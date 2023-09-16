d1887c15c390bdd1e4e0f1727a9f4753df4d57ee
JelenaVujin <persephone1805@gmail.com>
2023-09-16 06:24:14 +0200

    WorkoutTypes Model created

--- fitnessapp/database/migrations/2023_09_16_035423_create_workout_types_table.php
+++ fitnessapp/database/migrations/2023_09_16_035423_create_workout_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWorkoutsTable extends Migration
+class CreateWorkoutTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateWorkoutsTable extends Migration
      */
     public function up()
     {
-        Schema::create('workouts', function (Blueprint $table) {
+        Schema::create('workout_types', function (Blueprint $table) {
             $table->id();
-            $table->integer('duration');
-            $table->string('description');
+            $table->string('type');
             $table->timestamps();
-            $table->foreignId('workout_type_id');
-            $table->foreignId('trainer_id');
-            $table->foreignId('user_id');
-
+           
         });
     }
 
@@ -32,6 +28,6 @@ class CreateWorkoutsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('workouts');
+        Schema::dropIfExists('workout_types');
     }
 }
