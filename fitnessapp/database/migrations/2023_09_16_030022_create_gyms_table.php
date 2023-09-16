8c2105286250442399d7a57806a2e6b2e1f55798
JelenaVujin <persephone1805@gmail.com>
2023-09-16 05:01:58 +0200

    gym model created

---
+++ fitnessapp/database/migrations/2023_09_16_030022_create_gyms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGymsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gyms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('address')->unique();
+            $table->integer('phone_number')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gyms');
+    }
+}
